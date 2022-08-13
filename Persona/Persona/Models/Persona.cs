d489a070f593ba7c8d29b5e1b43f48f6db928cc2
Carlos1200 <carlos1200herrera@gmail.com>
2022-08-12 19:19:17 -0600

    Practica 1

---
+++ Persona/Persona/Models/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Persona.Models
+{
+    public class Persona
+    {
+        public int DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+    }
+    public class PersonaDBContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
